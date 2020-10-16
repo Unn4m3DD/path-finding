4d3726fe0a65e87ee27168fe76a70f84efa5cebe
André Patacas <guerreiroapatacas@gmail.com>
2020-10-16 23:30:39 +0100

    algorithm optimization and refactoring

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Point {
         this.y = y;
     }
 }
-const height = 150, width = 150;
+const height = 300, width = 300;
 let maze_container = document.getElementById("maze_container");
 let maze = [];
 let source = { x: 0, y: 0 };
@@ -17,7 +17,7 @@ const createMaze = () => {
     for (let x = 0; x < width; x++) {
         let inner = [];
         for (let y = 0; y < height; y++)
-            inner.push({ color_id: Math.random() > .3 ? 0 : 1, previous_node: { x: -1, y: -1 } });
+            inner.push({ ...(Math.random() > .3 ? { color: "#fff", wall: false } : { color: "#000", wall: true }), previous_node: { x: -1, y: -1 } });
         maze.push(inner);
     }
 };
@@ -35,14 +35,8 @@ const create_table = () => {
 const render = async () => {
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            if (maze[x][y].color_id === 1)
-                table.children[y].children[x].style.backgroundColor = "#000";
-            if (maze[x][y].color_id === 2)
-                table.children[y].children[x].style.backgroundColor = "blue";
-            if (maze[x][y].color_id === 3)
-                table.children[y].children[x].style.backgroundColor = "green";
-            if (maze[x][y].color_id === 4)
-                table.children[y].children[x].style.backgroundColor = "#aaa";
+            if (maze[x][y].color)
+                table.children[y].children[x].style.backgroundColor = maze[x][y].color;
             if (x === source.x && y === source.y)
                 table.children[y].children[x].style.backgroundColor = "green";
             if (x === target.x && y === target.y)
@@ -52,54 +46,67 @@ const render = async () => {
 };
 const selectPositions = () => {
     source = {
-        x: Math.round(Math.random() * width / 2),
-        y: Math.round(Math.random() * height / 2),
+        x: Math.round(Math.random() * width / 10),
+        y: Math.round(Math.random() * height / 10),
     };
-    while (maze[source.x][source.y].color_id != 0)
+    while (maze[source.x][source.y].wall)
         source = {
-            x: Math.round(Math.random() * width / 2),
-            y: Math.round(Math.random() * height / 2),
+            x: Math.round(Math.random() * width / 10),
+            y: Math.round(Math.random() * height / 10),
         };
     target = {
-        x: Math.round(Math.random() * width / 2 + width / 2 - 1),
-        y: Math.round(Math.random() * height / 2 + height / 2 - 1),
+        x: Math.round(Math.random() * width / 10 + 9 * width / 10 - 1),
+        y: Math.round(Math.random() * height / 10 + 9 * height / 10 - 1),
     };
-    while (maze[target.x][target.y].color_id != 0)
+    while (maze[target.x][target.y].wall)
         target = {
-            x: Math.round(Math.random() * width / 2 + width / 2 - 1),
-            y: Math.round(Math.random() * height / 2 + height / 2 - 1),
+            x: Math.round(Math.random() * width / 10 + 9 * width / 10 - 1),
+            y: Math.round(Math.random() * height / 10 + 9 * height / 10 - 1),
         };
 };
-const depth_first_search_rec = async (pos) => {
-    maze[pos.x][pos.y].color_id = 2;
+const depth_first_search_rec = async (pos, visited) => {
+    const inArray = (array, point) => {
+        for (let p of array)
+            if (p.x == point.x && p.y == point.y)
+                return true;
+    };
+    if (inArray(visited, pos))
+        return false;
+    visited.push(pos);
+    if (maze?.[pos.x]?.[pos.y])
+        maze[pos.x][pos.y].color = "#45f";
+    else
+        return false;
     if (pos.x === target.x && pos.y === target.y) {
         console.log(pos);
         return true;
     }
-    await sleep(0);
-    await render();
+    //await sleep(0)
+    //await render()
     for (let x = -1; x < 2; x += 2)
-        if (maze?.[pos.x + x]?.[pos.y]?.color_id === 0)
-            if (await depth_first_search_rec({ x: pos.x + x, y: pos.y })) {
-                maze[pos.x][pos.y].color_id = 3;
-                await sleep(0);
-                await render();
+        if (!maze?.[pos.x + x]?.[pos.y]?.wall)
+            if (await depth_first_search_rec({ x: pos.x + x, y: pos.y }, visited)) {
+                maze[pos.x][pos.y].color = "#4a7";
+                //await sleep(0)
+                //await render()
                 return true;
             }
     for (let y = -1; y < 2; y += 2)
-        if (maze?.[pos.x]?.[pos.y + y]?.color_id === 0)
-            if (await depth_first_search_rec({ x: pos.x, y: pos.y + y })) {
-                maze[pos.x][pos.y].color_id = 3;
-                await sleep(0);
-                await render();
+        if (!maze?.[pos.x]?.[pos.y + y]?.wall)
+            if (await depth_first_search_rec({ x: pos.x, y: pos.y + y }, visited)) {
+                maze[pos.x][pos.y].color = "#4a7";
+                //await sleep(0)
+                //await render()
                 return true;
             }
-    await render();
-    await sleep(0);
+    //await render()
+    //await sleep(0)
     return false;
 };
 const depth_first_search = async () => {
-    depth_first_search_rec(source);
+    await depth_first_search_rec(source, []);
+    await render();
+    await sleep(0);
 };
 const breadth_first_search = async () => {
     const inArray = (array, point) => {
@@ -108,32 +115,40 @@ const breadth_first_search = async () => {
                 return true;
     };
     let queue = [{ ...source, prev: { x: -1, y: -1 } }];
+    let visited = [];
     while (queue.length != 0) {
         let temp = queue.shift();
-        maze[temp.x][temp.y].color_id = 2;
+        if (inArray(visited, temp))
+            continue;
+        visited.push(temp);
+        if (!maze?.[temp.x]?.[temp.y])
+            continue;
+        maze[temp.x][temp.y].color = "#45f";
         maze[temp.x][temp.y].previous_node = temp.prev;
         if (temp.x == target.x && temp.y == target.y) {
             break;
         }
-        if (maze?.[temp.x + 1]?.[temp.y]?.color_id === 0 && !inArray(queue, { x: temp.x + 1, y: temp.y }))
+        if (!maze?.[temp.x + 1]?.[temp.y]?.wall)
             queue.push({ x: temp.x + 1, y: temp.y, prev: { x: temp.x, y: temp.y } });
-        if (maze?.[temp.x - 1]?.[temp.y]?.color_id === 0 && !inArray(queue, { x: temp.x - 1, y: temp.y }))
+        if (!maze?.[temp.x - 1]?.[temp.y]?.wall)
             queue.push({ x: temp.x - 1, y: temp.y, prev: { x: temp.x, y: temp.y } });
-        if (maze?.[temp.x]?.[temp.y + 1]?.color_id === 0 && !inArray(queue, { x: temp.x, y: temp.y + 1 }))
+        if (!maze?.[temp.x]?.[temp.y + 1]?.wall)
             queue.push({ x: temp.x, y: temp.y + 1, prev: { x: temp.x, y: temp.y } });
-        if (maze?.[temp.x]?.[temp.y - 1]?.color_id === 0 && !inArray(queue, { x: temp.x, y: temp.y - 1 }))
+        if (!maze?.[temp.x]?.[temp.y - 1]?.wall)
             queue.push({ x: temp.x, y: temp.y - 1, prev: { x: temp.x, y: temp.y } });
-        await sleep(0);
-        await render();
+        //await sleep(0)
+        //await render()
     }
     let current = maze[target.x][target.y];
     while (current.previous_node.x != -1) {
         if (maze?.[current.previous_node.x]?.[current.previous_node.y])
-            maze[current.previous_node.x][current.previous_node.y].color_id = 3;
+            maze[current.previous_node.x][current.previous_node.y].color = "#4a5";
         current = maze[current.previous_node.x][current.previous_node.y];
-        await sleep(0);
-        await render();
+        //await sleep(0)
+        //await render()
     }
+    await sleep(0);
+    await render();
 };
 const a_star = async () => {
     const inArray = (array, point) => {
@@ -141,39 +156,46 @@ const a_star = async () => {
             if (p.x == point.x && p.y == point.y)
                 return true;
     };
-    let queue = [{ ...source, prev: { x: -1, y: -1 } }];
+    const insert_in_place = (queue, item) => {
+        let i = 0;
+        while (i < queue.length && item.dist > queue[i++].dist)
+            ;
+        queue.splice(i, 0, item);
+    };
+    let queue = [{ ...source, prev: { x: -1, y: -1 }, dist: Math.abs(target.x - source.x) + Math.abs(target.y - source.y) }];
+    let visited = [];
     while (queue.length != 0) {
         let temp = queue.shift();
-        maze[temp.x][temp.y].color_id = 2;
+        if (inArray(visited, temp) || !maze?.[temp.x]?.[temp.y])
+            continue;
+        visited.push(temp);
+        maze[temp.x][temp.y].color = "#45f";
         maze[temp.x][temp.y].previous_node = temp.prev;
         if (temp.x == target.x && temp.y == target.y) {
             break;
         }
-        if (maze?.[temp.x + 1]?.[temp.y]?.color_id === 0 && !inArray(queue, { x: temp.x + 1, y: temp.y }))
-            queue.push({ x: temp.x + 1, y: temp.y, prev: { x: temp.x, y: temp.y } });
-        if (maze?.[temp.x - 1]?.[temp.y]?.color_id === 0 && !inArray(queue, { x: temp.x - 1, y: temp.y }))
-            queue.push({ x: temp.x - 1, y: temp.y, prev: { x: temp.x, y: temp.y } });
-        if (maze?.[temp.x]?.[temp.y + 1]?.color_id === 0 && !inArray(queue, { x: temp.x, y: temp.y + 1 }))
-            queue.push({ x: temp.x, y: temp.y + 1, prev: { x: temp.x, y: temp.y } });
-        if (maze?.[temp.x]?.[temp.y - 1]?.color_id === 0 && !inArray(queue, { x: temp.x, y: temp.y - 1 }))
-            queue.push({ x: temp.x, y: temp.y - 1, prev: { x: temp.x, y: temp.y } });
-        queue.sort((p1, p2) => ((p1.x - target.x) * (p1.x - target.x) + (p1.y - target.y) * (p1.y - target.y) <
-            (p2.x - target.x) * (p2.x - target.x) + (p2.y - target.y) * (p2.y - target.y) ?
-            -1 : 1));
-        await sleep(0);
-        await render();
+        if (!maze?.[temp.x + 1]?.[temp.y]?.wall)
+            insert_in_place(queue, ({ x: temp.x + 1, y: temp.y, prev: { x: temp.x, y: temp.y }, dist: Math.abs(target.x - temp.x + 1) + Math.abs(target.y - temp.y) }));
+        if (!maze?.[temp.x - 1]?.[temp.y]?.wall)
+            insert_in_place(queue, ({ x: temp.x - 1, y: temp.y, prev: { x: temp.x, y: temp.y }, dist: Math.abs(target.x - temp.x - 1) + Math.abs(target.y - temp.y) }));
+        if (!maze?.[temp.x]?.[temp.y + 1]?.wall)
+            insert_in_place(queue, ({ x: temp.x, y: temp.y + 1, prev: { x: temp.x, y: temp.y }, dist: Math.abs(target.x - temp.x) + Math.abs(target.y - temp.y + 1) }));
+        if (!maze?.[temp.x]?.[temp.y - 1]?.wall)
+            insert_in_place(queue, ({ x: temp.x, y: temp.y - 1, prev: { x: temp.x, y: temp.y }, dist: Math.abs(target.x - temp.x) + Math.abs(target.y - temp.y - 1) }));
+        //await sleep(0)
+        //await render()
     }
     let current = maze[target.x][target.y];
     while (current.previous_node.x != -1) {
         if (maze?.[current.previous_node.x]?.[current.previous_node.y])
-            maze[current.previous_node.x][current.previous_node.y].color_id = 3;
+            maze[current.previous_node.x][current.previous_node.y].color = "#4a5";
         current = maze[current.previous_node.x][current.previous_node.y];
-        await sleep(0);
-        await render();
     }
+    await sleep(0);
+    await render();
 };
 create_table();
 createMaze();
 selectPositions();
 render();
-a_star();
+breadth_first_search();
